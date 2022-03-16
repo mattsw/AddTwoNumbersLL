e433ee2d40bc0654f66f9873fd72a08de86197a2
mattsw <mattwilhelm@cet.com>
2022-03-15 17:13:04 -0700

    Initial commit with failing unit test

---
+++ AddTwoNumbers/AddTwoNumbers.Tests/AddTwoNumbers/AddTwoNumbersSolverTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers.Tests.AddTwoNumbers
+{
+    [TestClass]
+    public class AddTwoNumbersSolverTests
+    {
+        [TestMethod]
+        public void AddTwoNumbersSolver_ShouldBeAbleToAddTwoSimplenumbers()
+        {
+            var sut = new AddTwoNumbersSolver();
+            var list1 = new AddTwoNumbersSolver.ListNode(3);
+            var list2 = new AddTwoNumbersSolver.ListNode(1);
+            var result = sut.AddTwoNumbers(list1, list2);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.val);
+        }
+    }
+}
---
+++ AddTwoNumbers/AddTwoNumbers/AddTwoNumbersSolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    public sealed class AddTwoNumbersSolver
+    {
+        public sealed class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            { 
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return null;
+        }
+
+    }
+}
